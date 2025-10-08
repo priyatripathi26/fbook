025202e0e77c5415db7544fd9622b759cf4869d9
Priya Tripathi <priyatripathi@Mac.fritz.box>
2025-10-08 13:33:19 +0200

    json server config

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,12 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Auth {
-  
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  login(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
   isLoggedIn(): boolean {
     return !!localStorage.getItem('token');
+  }
+
+  getUsers() {
+    return this.http.get('/api/users');
   }
 
 }
